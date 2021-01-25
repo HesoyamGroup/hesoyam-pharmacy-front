1e1843272ca88111404a5d9a11dd89eca1b0812b
Nikola Dragic <nikola_dragic@hotmail.com>
2021-01-25 12:02:52 +0100

    feat: Login completed, axios client created.

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import LoginPage from '../views/LoginPage.vue'
 
+import {client} from '@/client/axiosClient'
+
 Vue.use(VueRouter)
 
 const routes = [
@@ -31,5 +33,34 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes
 })
+
+
+
+router.beforeEach( async (to, from, next) => {
+  //Before each routing, check user authentication (token)
+  validateAuthentication().then( (isAuthenticationValid) => {
+      if(!isAuthenticationValid){
+        clearUserData();
+      }
+      next();
+  });
+});
+
+function clearUserData(){
+  localStorage.removeItem('user_role');
+  localStorage.removeItem('user_token');
+  localStorage.removeItem('user_token_expires');
+}
+
+async function validateAuthentication() {
+    return await client({
+      method: 'GET',
+      url: 'auth/validate-token'
+    }).then( (response) => {
+      return true;
+    }, (error) => {
+      return false;
+    })
+}
 
 export default router
