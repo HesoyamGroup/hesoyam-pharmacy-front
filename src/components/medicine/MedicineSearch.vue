825ad2d79436bf7dab97c01bfe473bcfda5bf1a9
vucaj <vucaj1998@outlook.com>
2021-01-29 18:07:20 +0100

    feat: show table of pharmacies that contain selected medicine

--- src/components/medicine/MedicineSearch.vue
+++ src/components/medicine/MedicineSearch.vue
@@ -16,12 +16,59 @@
 
                             </v-select>
                         </v-col>
-                        <v-col justify='center' align='right' class="mt-3"  cols='3'>
+                        <v-col align='right' class="mt-5"  cols='3'>
                             <v-btn 
                             color="primary"
                             @click="searchPharmacies">
                                 Search
                             </v-btn>
+                        </v-col>
+                    </v-row>
+                    <v-row v-if="show">
+                        <v-col>
+                            <v-card>
+                                <v-card-title>
+                                    Pharmacies
+                                    <v-spacer></v-spacer>
+                                    <v-text-field
+                                        v-model="search"
+                                        append-icon="mdi-magnify"
+                                        label="Search"
+                                        single-line
+                                        hide-details
+                                    ></v-text-field>
+                                </v-card-title>
+                                <v-data-table
+                                    :headers='headers'
+                                    :items="pharmacies"
+                                    :item-key="selectedPharmacy.id"
+                                    :search="search"
+                                    v-model='selectedPharmacy'
+                                    show-select
+                                    :single-select='singleSelect'
+                                    return-object
+                                    >
+                                </v-data-table>
+                                <v-card-actions class="justify-center">
+                                    <v-btn
+                                    v-if='selectedPharmacy[0] != undefined'
+                                    color="primary"
+                                    @click="reserveMedicineDialog">
+                                        Reserve
+                                    </v-btn>
+                                </v-card-actions>
+                                <v-dialog v-model="dialog" max-width="500px">
+                                    <v-card>
+                                        <v-card-title class="headline">Select Pick-up Date</v-card-title>
+                                        <v-card-actions>
+                                        <v-spacer></v-spacer>
+                                        <v-btn color="blue darken-1" text @click="closeDialog">Cancel</v-btn>
+                                        <v-btn color="blue darken-1" text @click="reserveMedicine">Confirm</v-btn>
+                                        <v-spacer></v-spacer>
+                                        </v-card-actions>
+                                    </v-card>
+                                </v-dialog>
+                            </v-card>
                         </v-col>
                     </v-row>
             </v-card>
@@ -37,9 +84,22 @@ export default {
     name: 'MedicineSearch',
     data(){
         return {
+            //Search Bar
             allMedicine:[],
             selectedMedicine:{},
-            pharmacies: []
+            pharmacies: [],
+            //Pharmacies Table
+            show: false,
+            search: '',
+            selectedPharmacy:[],
+            singleSelect: true,
+            dialog: false,
+            headers: [
+            {text:'Name:', value:'name'},
+            {text: 'City', value:'address.city.cityName'},
+            {text: 'Address', value:'address.addressLine'},
+            {text: 'Rating', value:'rating'},
+            ],
         }
     },
     mounted(){
@@ -55,6 +115,11 @@ export default {
 
         })
     },
+    computed: {
+        filteredKeys () {
+        return this.keys.filter(key => key !== 'Name')
+        },
+    },
     methods:{
         searchPharmacies: function()
         {
@@ -66,11 +131,22 @@ export default {
             })
             .then((response) => {
                 vm.pharmacies = response.data;
+                vm.show = true;
             }, (error) => {
 
             })
 
+        },
+        reserveMedicineDialog: function()
+        {
+            console.log(this.selectedPharmacy[0].id)
+            this.dialog = true;
+        },
+        closeDialog: function()
+        {
+            this.dialog = false;
         }
+
     }
 
 }
