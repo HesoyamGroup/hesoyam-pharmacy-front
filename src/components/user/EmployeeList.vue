fc5358bec2353168f40fa43f66be1d0ecd6fba40
Gergo <49314980+gregVader@users.noreply.github.com>
2021-01-29 23:09:23 +0100

    feat: Filtering employees by rating or pharmacy name

--- src/components/user/EmployeeList.vue
+++ src/components/user/EmployeeList.vue
@@ -1,41 +1,73 @@
 <template>
-    <v-data-table
-    :headers="employeeHeaders"
-    :items="employees"
-    item-key="name"
-    :show-expand="expandTable"
-    class="elevation-1 ma-5">
+    <div>
+        <v-card>
+            <v-card-title>
+                {{tableTitle}}
+                <v-spacer></v-spacer>
+                <v-text-field class="mt-5 mr-1" label="First Name" v-model="firstName"></v-text-field>
+                <v-text-field class="mt-5 mr-1" label="Last Name" v-model="lastName"></v-text-field>
+                <v-btn @click="search">Search</v-btn>
+            </v-card-title>
+     
+            <v-data-table
+            :headers="employeeHeaders"
+            :items="filteredEmployees"
+            item-key="id"
+            :show-expand="expandTable"
+            :expanded.sync="expanded"
+            class="elevation-1 ma-5">
 
-    <template v-slot:top>
-      <v-toolbar flat>
-        <v-toolbar-title>{{tableTitle}}</v-toolbar-title>
-        <v-spacer></v-spacer>
-        <v-text-field class="mt-5 mr-1" label="First Name" v-model="firstName"></v-text-field>
-        <v-text-field class="mt-5 mr-1" label="Last Name" v-model="lastName"></v-text-field>
-        <v-btn @click="search">Search</v-btn>
-      </v-toolbar>
-    </template>
+            <template v-slot:top>
+            <v-toolbar flat>
+                <v-text-field
+                        v-model="filterRating"
+                        append-icon="mdi-magnify"
+                        label="Filter by rating"
+                        single-line
+                        hide-details
+                    ></v-text-field>
+                    <v-spacer></v-spacer>
+                    <v-text-field
+                        v-model="filterPharmacy"
+                        append-icon="mdi-magnify"
+                        label="Filter by pharmacy"
+                        single-line
+                        hide-details
+                    ></v-text-field>
+            </v-toolbar>
+            </template>
 
-    <template v-slot:expanded-item="{ headers, item }">
-        <td :colspan="headers.length">
-            <v-list-item-group color="primary">
-                <v-list-item v-for="pharmacy in item.pharmacies" :key="pharmacy.id">
-                    <v-list-item-content>
-                        <v-row>
-                            <v-col>
-                                <v-list-item-title>{{pharmacy.name}}</v-list-item-title>
-                            </v-col>
-                            <v-col>
-                                <v-btn :to="'/pharmacy/' + pharmacy.id"><v-icon>mdi-home-variant-outline</v-icon></v-btn>
-                            </v-col>
-                        </v-row>
-                    </v-list-item-content>
-                </v-list-item>
-            </v-list-item-group>
-        </td>
-    </template>
+            <template v-slot:item.rating="{ item }">
+            <v-chip
+                :color="getColor(item.rating)"
+                dark
+            >
+                {{ item.rating }}
+            </v-chip>
+            </template>
 
-    </v-data-table>
+            <template v-slot:expanded-item="{ headers, item }">
+                <td :colspan="headers.length">
+                    <v-list-item-group color="primary">
+                        <v-list-item v-for="pharmacy in item.pharmacies" :key="pharmacy.id">
+                            <v-list-item-content>
+                                <v-row>
+                                    <v-col>
+                                        <v-list-item-title>{{pharmacy.name}}</v-list-item-title>
+                                    </v-col>
+                                    <v-col>
+                                        <v-btn :to="'/pharmacy/' + pharmacy.id"><v-icon>mdi-home-variant-outline</v-icon></v-btn>
+                                    </v-col>
+                                </v-row>
+                            </v-list-item-content>
+                        </v-list-item>
+                    </v-list-item-group>
+                </td>
+            </template>
+
+            </v-data-table>
+        </v-card>
+    </div>
 </template>
 
 <script>
@@ -45,12 +77,20 @@ export default {
     //Props:
     //      type - must be either pharmacist or dermatologist
     name: 'EmployeeList',
-    props: ['type'],
+    props: {
+        type:{
+            type: String,
+            required: true
+        }
+    },
     data: function(){
         return {
             firstName: '',
             lastName: '',
             employees: [],
+            expanded: [],
+            filterRating: '',
+            filterPharmacy: '',
             pharmacyHeaders: [
                 {
                     text: 'Pharmacy',
@@ -91,9 +131,26 @@ export default {
             }, (error) => {
 
             })
+        },
+        getColor (calories) {
+            if (calories >= 4) return 'green'
+            else if (calories >= 2) return 'orange'
+            else return 'red'
+        },
+        isPharmacyNameSatisfied(employee, pharmacyName){
+            let result = false;
+            if(this.type == 'pharmacist')
+                result = employee.pharmacy.name.toLowerCase().indexOf(pharmacyName.toLowerCase()) !== -1;
+            else
+                employee.pharmacies.forEach(ph => ph.name.toLowerCase().indexOf(pharmacyName.toLowerCase()) !== -1 ? result = true : {});
+            
+            return result;
         }
     },
     computed: {
+        filteredEmployees(){
+            return this.employees.filter(e => e.rating.toString().indexOf(this.filterRating.toLowerCase()) !== -1 && this.isPharmacyNameSatisfied(e, this.filterPharmacy));
+        },
         expandTable(){
             return this.type == 'dermatologist';
         },
