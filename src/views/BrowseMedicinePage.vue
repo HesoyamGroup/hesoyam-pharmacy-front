403da908d01b7d0db9029709e0caa273c0ab9913
Nikola Dragic <nikola_dragic@hotmail.com>
2021-02-01 17:36:41 +0100

    feat: Browse/Search medicine

---
+++ src/views/BrowseMedicinePage.vue
@@ -0,0 +1,22 @@
+<template> 
+    <div id="supplier-page">
+        <browse-medicine> </browse-medicine>
+        
+    </div>
+</template>
+
+
+<script>
+
+    import BrowseMedicine from '@/components/medicine/BrowseMedicine.vue';
+
+    export default {
+        name: 'browse-medicine-page',
+        components: {
+            BrowseMedicine
+        }
+    }
+</script>
+
+<style scoped>
+</style>
